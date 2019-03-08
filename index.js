08b7c432054eb11de1d9cdd3a0b9bc2d927790a9
Joanna Yuhas <joannayuhas@gmail.com>
2019-03-08 16:47:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ function happyHolidayTo(holiday, name){
   return `Happy ${holiday}, ${name}!`
 }
 
+var holiday ="Independence Day"
+var name = "you"
+
 function holidayCountdown(holiday, days){
   return `It's ${days} days until ${holiday}!`
 }
